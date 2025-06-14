28039ddb3f692125e579e616f453dee38e28c992
Justinshao33 <psp940929@gmail.com>
2025-06-14 16:49:46 +0800

    update Geometry

---
+++ code/Geometry/IntersectionOfLine.cpp
@@ -0,0 +1,19 @@
+bool isInter(Line l, Line m) {
+    if (PtOnSeg(m.a, l) or PtOnSeg(m.b, l) or
+        PtOnSeg(l.a, m) or PtOnSeg(l.b, m))
+        return true;
+    return PtSide(m.a, l) * PtSide(m.b, l) < 0 and
+           PtSide(l.a, m) * PtSide(l.b, m) < 0;
+}
+Pt LineInter(Line l, Line m) {
+    double s = ori(m.a, m.b, l.a), t = ori(m.a, m.b, l.b);
+    return (l.b * s - l.a * t) / (s - t);
+}
+bool strictInter(Line l, Line m) {
+    int la = PtSide(m.a, l);
+    int lb = PtSide(m.b, l);
+    int ma = PtSide(l.a, m);
+    int mb = PtSide(l.b, m);
+    if (la == 0 and lb == 0) return false;
+    return la * lb < 0 and ma * mb < 0;
+}
