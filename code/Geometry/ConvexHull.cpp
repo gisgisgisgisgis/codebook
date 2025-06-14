e291d66b36793e89c3800053663f93e5876d8428
Justinshao33 <psp940929@gmail.com>
2025-06-14 16:46:11 +0800

    add Point Line ConvexHull

---
+++ code/Geometry/ConvexHull.cpp
@@ -0,0 +1,18 @@
+vector<Pt> BuildHull(vector<Pt> pt) {
+    sort(all(pt));
+    pt.erase(unique(all(pt)), pt.end());
+    if (pt.size() <= 2) return pt;
+    vector<Pt> hull;
+    int sz = 1;
+    rep (t, 0, 2) {
+        rep (i, t, ssize(pt)) {
+            while (ssize(hull) > sz && ori(hull.end()[-2], pt[i], hull.back()) >= 0)
+                hull.pop_back();
+            hull.pb(pt[i]);
+        }
+        sz = ssize(hull);
+        reverse(all(pt));
+    }
+    hull.pop_back();
+    return hull;
+}
