175e2974fc9db3364982f8c22987159c6c833619
CuteWhiteCat <whitecat.cs13@nycu.edu.tw>
2025-06-15 23:42:46 +0800

    Basic File and Simplex

---
+++ code/Basic/tem.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+using i64 = long long;
+
+#define int i64
+#define rep(x, y, z) for (int x = (y); x <= (z); x++)
+#define all(x) x.begin(), x.end()
+#define pb push_back
+
+bool chmin(auto &a, auto b) { return (b < a and (a = b, true)); }
+bool chmax(auto &a, auto b) { return (a < b and (a = b, true)); }
+
+void solve() {
+    //
+}
+
+int32_t main() {
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+
+    int t = 1;
+    std::cin >> t;
+
+    while (t--) {
+        solve();
+    }
+
+    return 0;
+}
