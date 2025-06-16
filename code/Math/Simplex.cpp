6fe8ef9681b7d3ea4a0b95d52dc3665c24f0b7f9
CuteWhiteCat <whitecat.cs13@nycu.edu.tw>
2025-06-17 02:24:09 +0800

    fix bugs in Simplex

--- code/Math/Simplex.cpp
+++ code/Math/Simplex.cpp
@@ -1,26 +1,20 @@
-// a : Matrix; b, c: Vector
-// Ax <= b, max c^T x
-// T: double or long double
+// a : Matrix; b, c : Vector
+// max cᵀx subject to (Ax <= b and x >= 0)
+// return {state, opt, solVector}
+//   state = 0 → optimal
+//   state = 1 → unbounded
+//   state = 2 → infeasible
 template <typename T>
 struct Simplex {
     const T eps = 1E-7;
     const T inf = std::numeric_limits<T>::infinity();
-
-    int n, m, vars, cols;
+    int n, m, V, C;
     std::vector<std::vector<T>> A;
     std::vector<int> basis;
 
-    Simplex(auto& a, auto& b, auto& c) {
-        init(a, b, c);
-    }
-
-    void init(auto& a, auto& b, auto& c) {
-        n = b.size(), m = c.size();
-        vars = n + m;
-        cols = vars + 1;
-
-        A.assign(n + 1, std::vector<T>(cols));
-
+    Simplex(const std::vector<std::vector<T>>& a,
+            const std::vector<T>& b,
+            const std::vector<T>& c) : n(b.size()), m(c.size()), V(n + m), C(V + 1), A(n + 1, std::vector<T>(C)), basis(n) {
         for (int i = 0; i < n; i++) {
             ranges::copy(a[i], A[i].begin());
             A[i][m + i] = 1.0;
@@ -29,32 +23,37 @@ struct Simplex {
         ranges::transform(c, A[n].begin(), [](T x) {
             return -x;
         });
-
-        basis.assign(n, 0);
         ranges::iota(basis, m);
     }
 
     void pivot(int r, int s) {
-        T inv = 1.0 / A[r][s];
+        const T inv = 1.0 / A[r][s];
         for (auto& x : A[r]) {
             x *= inv;
         }
         for (int i = 0; i <= n; i++) {
             if (i != r && std::fabs(A[i][s]) > eps) {
-                T f = std::exchange(A[i][s], 0);
-                for (int j = 0; j < cols; j++) {
-                    A[i][j] -= f * A[r][j];
+                const T f = std::exchange(A[i][s], 0);
+                for (int j = 0; j < C; j++) {
+                    if (j != s) {
+                        A[i][j] -= f * A[r][j];
+                    }
                 }
             }
         }
         basis[r] = s;
     }
-
+    
     auto work() {
+        for (int i = 0; i < n; i++) {
+            if (A[i].back() < -eps) {
+                return std::tuple(2, inf, std::vector<T>{});
+            }
+        }
         while (true) {
-            T res = inf;
             int s = -1, r = -1;
-            for (int i = 0; i < vars; i++) {
+            T res = inf;
+            for (int i = 0; i < V; i++) {
                 if (A.back()[i] < -eps) {
                     s = i;
                     break;
@@ -67,24 +66,21 @@ struct Simplex {
                 if (A[i][s] > eps) {
                     T ratio = A[i].back() / A[i][s];
                     if (ratio < res - eps) {
-                        res = ratio;
-                        r = i;
+                        res = ratio, r = i;
                     }
                 }
             }
             if (r == -1) {
-                return std::pair(std::vector<T>{}, -1.0);
+                return std::tuple(1, inf, std::vector<T>{});
             }
             pivot(r, s);
         }
-
         std::vector<T> X(m);
         for (int i = 0; i < n; i++) {
             if (basis[i] < m) {
                 X[basis[i]] = A[i].back();
             }
         }
-
-        return std::pair(X, A[n].back());
+        return std::tuple(0, A[n].back(), std::move(X));
     }
 };
