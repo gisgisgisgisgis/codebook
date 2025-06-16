6b4bcc5e774773e27f2639d0b60d5d523160d321
CuteWhiteCat <whitecat.cs13@nycu.edu.tw>
2025-06-16 18:31:41 +0800

    Minor fix

--- code/Basic/run.bat
+++ code/Basic/run.bat
@@ -1,3 +1,3 @@
 @echo off
-g++ -std=c++23 -DLOCAL -Wall -Wextra -Wshadow -O2 %1.cpp -o %1.exe
+g++ -std=c++23 -D LOCAL -Wall -Wextra -Wshadow -O2 %1.cpp -o %1.exe
 if "%2" == "" ("%1.exe") else ("%1.exe" < "%2")
