c6f33849e52498fe5e3886049de7a2774dc15f07
Justinshao33 <psp940929@gmail.com>
2025-06-17 20:24:35 +0800

    fix some coding style

--- code/Flow/MCMF.cpp
+++ code/Flow/MCMF.cpp
@@ -1,18 +1,19 @@
 template<class T>
 struct MCMF {
-    struct edge { int v; T f, w; int rev; };
+    const T INF = numeric_limits<T>::max() / 2;
+    struct edge { int v, r; T f, w; };
     vector<vector<edge>> adj;
     const int n;
     MCMF(int n) : n(n), adj(n) {}
     void addEdge(int u, int v, T f, T c) {
-        adj[u].push_back({v, f, c, ssize(adj[v])});
-        adj[v].push_back({u, 0, -c, ssize(adj[u]) - 1});
+        adj[u].push_back({v, ssize(adj[v]), f, c});
+        adj[v].push_back({u, ssize(adj[u]) - 1, 0, -c});
     }
     vector<T> dis;
     vector<bool> vis;
     bool spfa(int s, int t) {
         queue<int> que;
-        dis.assign(n, inf<T>);
+        dis.assign(n, INF);
         vis.assign(n, false);
         que.push(s);
         vis[s] = 1;
@@ -20,20 +21,20 @@ struct MCMF {
         while (!que.empty()) {
             int u = que.front(); que.pop();
             vis[u] = 0;
-            for (auto [v, f, w, _] : adj[u])
+            for (auto [v, _, f, w] : adj[u])
                 if (f && chmin(dis[v], dis[u] + w))
                     if (!vis[v]) {
                         que.push(v);
                         vis[v] = 1;
                     }
         }
-        return dis[t] != inf<T>;
+        return dis[t] != INF;
     }
     T dfs(int u, T in, int t) {
         if (u == t) return in;
         vis[u] = 1;
         T out = 0;
-        for (auto &[v, f, w, rev] : adj[u])
+        for (auto &[v, rev, f, w] : adj[u])
             if (f && !vis[v] && dis[v] == dis[u] + w) {
                 T x = dfs(v, min(in, f), t);
                 in -= x;
@@ -42,14 +43,14 @@ struct MCMF {
                 adj[v][rev].f += x;
                 if (!in) break;
             }
-        if (in) dis[u] = inf<T>; 
+        if (in) dis[u] = INF; 
         vis[u] = 0;
         return out;
     }
     pair<T, T> flow(int s, int t) { // {flow, cost}
         T a = 0, b = 0;
         while (spfa(s, t)) {
-            T x = dfs(s, inf<T>, t);
+            T x = dfs(s, INF, t);
             a += x;
             b += x * dis[t];
         }
