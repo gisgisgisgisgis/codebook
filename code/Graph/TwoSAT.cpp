097b59d1553f0660b6e31751ace67ff5dcc69fca
Justinshao33 <psp940929@gmail.com>
2025-06-19 01:48:18 +0800

    add twoSAT

---
+++ code/Graph/TwoSAT.cpp
@@ -0,0 +1,51 @@
+struct TwoSat {
+    int n;
+    vector<vector<int>> G;
+    vector<bool> ans;
+    vector<int> id, dfn, low, stk;
+    TwoSat(int n) : n(n), G(2 * n), ans(n), 
+        id(2 * n, -1), dfn(2 * n, -1), low(2 * n, -1) {}
+    void addClause(int u, bool f, int v, bool g) { // (u = f) or (v = g)
+        G[2 * u + !f].push_back(2 * v + g);
+        G[2 * v + !g].push_back(2 * u + f);
+    }
+    void addImply(int u, bool f, int v, bool g) { // (u = f) -> (v = g)
+        G[2 * u + f].push_back(2 * v + g);
+        G[2 * v + !g].push_back(2 * u + !f);
+    }
+    int cur = 0, scc = 0;
+    void dfs(int u) {
+        stk.push_back(u);
+        dfn[u] = low[u] = cur++;
+        for (int v : G[u]) {
+            if (dfn[v] == -1) {
+                dfs(v);
+                chmin(low[u], low[v]);
+            } else if (id[v] == -1) {
+                chmin(low[u], dfn[v]);
+            }
+        }
+        if (dfn[u] == low[u]) {
+            int x;
+            do {
+                x = stk.back();
+                stk.pop_back();
+                id[x] = scc;
+            } while (x != u);
+            scc++;
+        }
+    }
+    bool satisfiable() {
+        for (int i = 0; i < n * 2; i++)
+            if (dfn[i] == -1) {
+                dfs(i);
+            }
+        for (int i = 0; i < n; ++i) {
+            if (id[2 * i] == id[2 * i + 1]) {
+                return false;
+            }
+            ans[i] = id[2 * i] > id[2 * i + 1];
+        }
+        return true;
+    }
+};
