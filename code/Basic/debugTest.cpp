f796b60d5d298e375dbf4930603a188b23581622
白貓 <95128161+CuteWhiteCat@users.noreply.github.com>
2025-06-30 01:21:50 +0800

    Update debugTest.cpp

--- code/Basic/debugTest.cpp
+++ code/Basic/debugTest.cpp
@@ -5,17 +5,17 @@ namespace ranges = std::ranges;
 template <typename R>
 concept I = ranges::range<R> && !std::same_as<ranges::range_value_t<R>, char>;
 template <typename A, typename B>
-std::ostream& operator<<(std::ostream& o, const std::pair<A, B>& p) {
+std::ostream &operator<<(std::ostream &o, const std::pair<A, B> &p) {
     return o << "(" << p.first << ", " << p.second << ")";
 }
 template <I T>
-std::ostream& operator<<(std::ostream& o, const T& v) {
+std::ostream &operator<<(std::ostream &o, const T &v) {
     o << "{";
     int f = 0;
     for (auto &&i : v) o << (f++ ? " " : "") << i;
     return o << "}";
 }
-void debug__(int c, auto&&... a) {
+void debug__(int c, auto &&...a) {
     std::cerr << "\e[1;" << c << "m";
     (..., (std::cerr << a << " "));
     std::cerr << "\e[0m" << std::endl;
