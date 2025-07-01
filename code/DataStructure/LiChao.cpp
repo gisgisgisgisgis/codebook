27e4047cb5c0ffd1660780cf55d283d9d22270c1
Justinshao33 <psp940929@gmail.com>
2025-07-01 13:04:35 +0800

    upd LiChao

--- code/DataStructure/LiChao.cpp
+++ code/DataStructure/LiChao.cpp
@@ -1,48 +1,47 @@
 struct Line {
-    int m, k = -INF;
-    Line() {}
-    Line(int _m, int _k) : m(_m), k(_k) {}
-    int operator () (const int &x) const {
-        return m * x + k;
+    // y = ax + b
+    i64 a{0}, b{-inf<i64>};
+    i64 operator()(i64 x) {
+        return a * x + b;
     }
 };
 // max LiChao
-struct Node {
-    Line line;
-    int ls, rs;
-    Node() {}
-    Node(Line li) : ls(0), rs(0), line(li) {}
-};
-struct LiChao {  
-    vector<Node> tree;
-    LiChao () : tree(1) {};
-    int newnode(Line line) {
-        tree.push_back(Node(line));
-        return ssize(tree) - 1;
-    }
-    void insert (int &pos, int l, int r, Line line) {
-        if (pos == 0) {
-            pos = newnode(line);
+struct Seg {
+    int l, r;
+    Seg *ls{}, *rs{};
+    Line f{};
+    Seg(int l, int r) : l(l), r(r) {}
+    void add(Line g) {
+        int m = (l + r) / 2;
+        if (g(m) > f(m)) {
+            swap(g, f);
+        }
+        if (g.b == -inf<i64> or r - l == 1) {
             return;
         }
-        auto &tr = tree[pos].line;
-        if (l == r) {
-            if (line(l) > tr(l)) tr = line;
-            return;
-        }
-        int mid = l + r >> 1;
-        if (tr(mid) < line(mid)) swap(tr, line);
-        if (tr.m >= line.m) {
-            insert(tree[pos].ls, l, mid, line);
+        if (g.a < f.a) {
+            if (!ls) {
+                ls = new Seg(l, m);
+            }
+            ls->add(g);
         } else {
-            insert(tree[pos].rs, mid + 1, r, line);
+            if (!rs) {
+                rs = new Seg(m, r);
+            }
+            rs->add(g);
         }
     }
-    int query (int pos, int l, int r, int x) {
-        if (pos == 0) return -INF;
-        if (l == r) return tree[pos].line(x);
-        int mid = l + r >> 1;
-        if (x <= mid) return max(query(tree[pos].ls, l, mid, x), tree[pos].line(x));
-        else return max(query(tree[pos].rs, mid + 1, r, x), tree[pos].line(x));
+    i64 qry(i64 x) {
+        if (f.b == -inf<i64>) {
+            return -inf<i64>;
+        }
+        int m = (l + r) / 2;
+        i64 y = f(x);
+        if (x < m and ls) {
+            chmax(y, ls->qry(x));
+        } else if (x >= m and rs) {
+            chmax(y, rs->qry(x));
+        }
+        return y;
     }
 };
