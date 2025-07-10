904b2da24ba1f5f53185c264783a2472c410aabc
Justinshao33 <psp940929@gmail.com>
2025-07-10 08:03:58 +0800

    upd TwoSat addAtMostOne

--- code/Graph/TwoSAT.cpp
+++ code/Graph/TwoSAT.cpp
@@ -3,8 +3,7 @@ struct TwoSat {
     vector<vector<int>> G;
     vector<bool> ans;
     vector<int> id, dfn, low, stk;
-    TwoSat(int n) : n(n), G(2 * n), ans(n), 
-        id(2 * n, -1), dfn(2 * n, -1), low(2 * n, -1) {}
+    TwoSat(int n) : n(n), G(2 * n) {}
     void addClause(int u, bool f, int v, bool g) { // (u = f) or (v = g)
         G[2 * u + !f].push_back(2 * v + g);
         G[2 * v + !g].push_back(2 * u + f);
@@ -12,6 +11,24 @@ struct TwoSat {
     void addImply(int u, bool f, int v, bool g) { // (u = f) -> (v = g)
         G[2 * u + f].push_back(2 * v + g);
         G[2 * v + !g].push_back(2 * u + !f);
+    }
+    int addVar() {
+        G.emplace_back();
+        G.emplace_back();
+        return n++;
+    }
+    void addAtMostOne(const vector<pair<int, bool>> &li) {
+        if (ssize(li) <= 1) return;
+        int pu; bool pf; tie(pu, pf) = li[0];
+        for (int i = 2; i < ssize(li); i++) {
+            const auto &[u, f] = li[i];
+            int nxt = addVar();
+            addClause(pu, !pf, u, !f);
+            addClause(pu, !pf, nxt, true);
+            addClause(u, !f, nxt, true);
+            tie(pu, pf) = make_pair(nxt, true);
+        }
+        addClause(pu, !pf, li[1].first, !li[1].second);
     }
     int cur = 0, scc = 0;
     void dfs(int u) {
@@ -36,6 +53,10 @@ struct TwoSat {
         }
     }
     bool satisfiable() {
+        ans.assign(n, 0);
+        id.assign(2 * n, -1);
+        dfn.assign(2 * n, -1);
+        low.assign(2 * n, -1);
         for (int i = 0; i < n * 2; i++)
             if (dfn[i] == -1) {
                 dfs(i);
