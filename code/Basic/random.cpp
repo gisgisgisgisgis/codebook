099ae020f685f6f8633d5503b8ba9c5e5bf3313b
白貓 <95128161+CuteWhiteCat@users.noreply.github.com>
2025-07-30 01:30:26 +0800

    Update random.cpp

--- code/Basic/random.cpp
+++ code/Basic/random.cpp
@@ -1,3 +1,2 @@
 std::mt19937_64 rng(std::chrono::steady_clock::now().time_since_epoch().count());
-inline int rand(int l, int r) { return std::uniform_int_distribution<int>(l, r)(rng); }
 inline i64 rand(i64 l, i64 r) { return std::uniform_int_distribution<i64>(l, r)(rng); }
