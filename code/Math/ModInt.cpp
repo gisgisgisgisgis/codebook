8c46d9d3c15ecf8bc26fae4b5a542d1fdf0e662b
白貓 <95128161+CuteWhiteCat@users.noreply.github.com>
2025-08-04 06:15:18 +0800

    Update ModInt.cpp

--- code/Math/ModInt.cpp
+++ code/Math/ModInt.cpp
@@ -10,29 +10,32 @@ constexpr T power(T a, u64 b, T res = 1) {
     return res;
 }
 
-template <u32 P = 998244353>
+template <u32 P>
 struct ModInt {
     u32 v;
     const static ModInt G;
-    ModInt(i64 x = 0) { norm(x % P + P); }
-    ModInt &norm(u32 x) {
+
+    constexpr ModInt &norm(u32 x) {
         v = x < P ? x : x - P;
         return *this;
     }
-    ModInt inv() const { return power(*this, P - 2); }
-    ModInt operator+(const ModInt &r) const { return ModInt().norm(v + r.v); }
-    ModInt operator-(const ModInt &r) const { return ModInt().norm(v + P - r.v); }
-    ModInt operator*(const ModInt &r) const { return ModInt().norm(u64(v) * r.v % P); }
-    ModInt operator/(const ModInt &r) const { return *this * r.inv(); }
-    ModInt &operator+=(const ModInt &r) { return *this = *this + r; }
-    ModInt &operator-=(const ModInt &r) { return *this = *this - r; }
-    ModInt &operator*=(const ModInt &r) { return *this = *this * r; }
-    ModInt &operator/=(const ModInt &r) { return *this = *this / r; }
-    bool operator==(const ModInt &r) const { return v == r.v; }
-    explicit operator bool() const { return v != 0; }
+    constexpr ModInt(i64 x = 0) { norm(x % P + P); }
+    constexpr ModInt inv() const { return power(*this, P - 2); }
+    constexpr ModInt operator-() const { return ModInt() - *this; }
+    constexpr ModInt operator+(const ModInt &r) const { return ModInt().norm(v + r.v); }
+    constexpr ModInt operator-(const ModInt &r) const { return ModInt().norm(v + P - r.v); }
+    constexpr ModInt operator*(const ModInt &r) const { return ModInt().norm(u64(v) * r.v % P); }
+    constexpr ModInt operator/(const ModInt &r) const { return *this * r.inv(); }
+    constexpr ModInt &operator+=(const ModInt &r) { return *this = *this + r; }
+    constexpr ModInt &operator-=(const ModInt &r) { return *this = *this - r; }
+    constexpr ModInt &operator*=(const ModInt &r) { return *this = *this * r; }
+    constexpr ModInt &operator/=(const ModInt &r) { return *this = *this / r; }
+    constexpr bool operator==(const ModInt &r) const { return v == r.v; }
+    constexpr bool operator!=(const ModInt &r) const { return v != r.v; }
+    explicit constexpr operator bool() const { return v != 0; }
     friend std::ostream &operator<<(std::ostream &os, const ModInt &r) {
         return os << r.v;
     }
 };
 using mint = ModInt<998244353>;
-template<> const mint mint::G = mint(3);
+template <> const mint mint::G = mint(3);
