7ec23e3f19da15bb48a6db1d39f13908e608e9a0
Justinshao33 <psp940929@gmail.com>
2025-08-11 04:13:37 +0800

    pbds

---
+++ code/DataStructure/PBDS.cpp
@@ -0,0 +1,5 @@
+#include<bits/extc++.h>
+using namespace __gnu_pbds;
+template<typename T> using RBT = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+// tree.order_of_key();
+// *tree.find_by_order();
