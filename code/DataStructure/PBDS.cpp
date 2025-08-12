0e97417f3b61aeb90b46bf656d6ca9f04341ff75
Justinshao33 <psp940929@gmail.com>
2025-08-12 16:30:47 +0800

    gp_hash and Graphs

--- code/DataStructure/PBDS.cpp
+++ code/DataStructure/PBDS.cpp
@@ -1,5 +1,22 @@
-#include<bits/extc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
 using namespace __gnu_pbds;
 template<typename T> using RBT = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
-// tree.order_of_key();
-// *tree.find_by_order();
+/*
+.find_by_order(k) 回傳第 k 小的值（based-0）
+.order_of_key(k) 回傳有多少元素比 k 小
+*/
+struct custom_hash {
+    static uint64_t splitmix64(uint64_t x) {
+        x += 0x9e3779b97f4a7c15;
+        x = (x ^ (x >> 30)) * 0xbf58476d1ce4e5b9;
+        x = (x ^ (x >> 27)) * 0x94d049bb133111eb;
+        return x ^ (x >> 31);
+    }
+
+    size_t operator()(uint64_t x) const {
+        static const uint64_t FIXED_RANDOM = chrono::steady_clock::now().time_since_epoch().count();
+        return splitmix64(x + FIXED_RANDOM);
+    }
+};
+// gp_hash_table<int, int, custom_hash> ss;
---
+++ code/Graph/EnumeratePlanarFace.cpp
@@ -0,0 +1,57 @@
+// 0-based
+struct PlanarGraph{
+    int n, m, id;
+    vector<Pt<int>> v;
+    vector<vector<pair<int, int>>> adj;
+    vector<int> conv, nxt, vis;
+
+    PlanarGraph(int n, int m, vector<Pt<int>> _v) : 
+    n(n), m(m), id(0),
+    v(_v), adj(n),
+    conv(m << 1), nxt(m << 1), vis(m << 1) {}
+
+    void add_edge(int x, int y) {
+        adj[x].push_back({y, id << 1});
+        adj[y].push_back({x, id << 1 | 1});
+        conv[id << 1] = x;
+        conv[id << 1 | 1] = y;
+        id++;
+    }
+
+    vector<int> enumerate_face() {
+        for (int i = 0; i < n; i++) {
+            sort(all(adj[i]), [&](const auto &a, const auto & b) {
+                return (v[a.first] - v[i]) < (v[b.first] - v[i]);
+            });
+            int sz = adj[i].size(), pre = sz - 1;
+            for (int j = 0; j < sz; j++) {
+                nxt[adj[i][pre].second] = adj[i][j].second ^ 1;
+                pre = j;
+            }
+        }
+
+        vector<int> ret;
+        for (int i = 0; i < m * 2; i++) {
+            if (!vis[i]) {
+                int area = 0, now = i;
+                vector<int> pt;
+                while (!vis[now]) {
+                    vis[now] = true;
+                    pt.push_back(conv[now]);
+                    now = nxt[now];
+                }
+                pt.push_back(pt.front());
+                for (int i = 0; i + 1 < ssize(pt); i++) {
+                    area -= (v[pt[i]] ^ v[pt[i + 1]]);
+                }
+                // pt = face boundary
+                if (area > 0) {
+                    ret.push_back(area);
+                } else {
+                    // pt is outer face
+                }
+            }
+        }
+        return ret;
+    }
+};
