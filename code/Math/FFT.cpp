1a54928a3e5755370a9af77decab8e9ff73f622f
Justinshao33 <psp940929@gmail.com>
2025-08-12 17:02:18 +0800

    add FFT

---
+++ code/Math/FFT.cpp
@@ -0,0 +1,34 @@
+template<typename C = complex<double>>
+void FFT(vector<C> &P, C w, bool inv = 0) {
+    int n = P.size(), lg = __builtin_ctz(n);
+    assert(__builtin_popcount(n) == 1);
+
+    for (int j = 1, i = 0; j < n - 1; ++j) {
+        for (int k = n >> 1; k > (i ^= k); k >>= 1);
+        if (j < i) swap(P[i], P[j]);
+    }
+
+    vector<C> ws = {inv ? C{1} / w : w};
+
+    rep (i, 1, lg) ws.pb(ws[i - 1] * ws[i - 1]);
+    reverse(all(ws));
+
+    rep (i, 0, lg) {
+        for (int k = 0; k < n; k += 2 << i) {
+            C base = C{1};
+            rep (j, k, k + (1 << i)) {
+                auto t = base * P[j + (1 << i)];
+                auto u = P[j];
+                P[j] = u + t;
+                P[j + (1 << i)] = u - t;
+                base = base * ws[i];
+            }
+        }
+    }
+
+    if (inv) rep (i, 0, n) P[i] = P[i] / C(n);
+}
+
+const int N = 1 << 21;
+const double PI = acos(-1);
+const auto w = exp(-complex<double>(0, 2.0 * PI / N));
