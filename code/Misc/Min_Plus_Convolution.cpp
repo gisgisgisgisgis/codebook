c4d5521a7cf18e402fb3fdbbfd9793015d87da50
Justinshao33 <psp940929@gmail.com>
2025-08-24 16:52:40 +0800

    fix MinPlus

--- code/Misc/Min_Plus_Convolution.cpp
+++ code/Misc/Min_Plus_Convolution.cpp
@@ -1,11 +1,3 @@
-#include<bits/stdc++.h>
-using namespace std;
-
-#define int long long
-#define rep(a, b, c) for (int a = b; a < c; a++)
-
-const int INF = 1e18;
-
 // a is convex a[i+1]-a[i] <= a[i+2]-a[i+1]
 vector<int> min_plus_convolution(vector<int> &a, vector<int> &b) {
   int n = ssize(a), m = ssize(b);
@@ -20,14 +12,4 @@ vector<int> min_plus_convolution(vector<int> &a, vector<int> &b) {
     Y(Y, l, mid - 1, jl, from), Y(Y, mid + 1, r, from, jr);
   };
   return dc(dc, 0, n - 1 + m - 1, 0, m - 1), c;
-}
-
-signed main() {
-    ios_base::sync_with_stdio(0), cin.tie(0);
-    int n, m; cin >> n >> m;
-    vector<int> a(n), b(m);
-    rep (i, 0, n) cin >> a[i];
-    rep (i, 0, m) cin >> b[i];
-    auto ans = min_plus_convolution(a, b);
-    rep (i, 0, n + m - 1) cout << ans[i] << " \n" [i == n + m - 2];
 }
