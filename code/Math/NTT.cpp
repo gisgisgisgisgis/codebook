e8a5c5de37c33a8463a3ab1ed8a2a70017087a47
Justinshao33 <psp940929@gmail.com>
2025-08-26 08:36:44 +0800

    NTT

--- code/Math/NTT.cpp
+++ code/Math/NTT.cpp
@@ -1,27 +1,34 @@
-template<typename M = ll>
-void NTT(vector<M> &P, M w, bool inv = 0) {
-    int n = P.size(), lg = __builtin_ctz(n);
-    assert(__builtin_popcount(n) == 1);
-    for (int j = 1, i = 0; j < n - 1; ++j) {
-        for (int k = n >> 1; k > (i ^= k); k >>= 1); // !!!
-        if (j < i) swap(P[i], P[j]);
+// add sub mul
+struct ntt {
+    vector<int> ws;
+    ntt(int N) : ws(N) {
+        int wb = fpow(3, (MOD - 1) / N, MOD);
+        ws[0] = 1;
+        rep (i, 1, N) ws[i] = mul(ws[i - 1], wb);
     }
-    vector<M> ws = {inv ? M{1} * fpow(w, MOD - 2, MOD) : w};
-    rep (i, 1, lg) ws.pb(ws[i - 1] * ws[i - 1] % MOD);
-    reverse(all(ws));
-    rep (i, 0, lg) {
-        for (int k = 0; k < n; k += 2 << i) {
-            M base = M{1};
-            rep (j, k, k + (1 << i)) {
-                auto t = base * P[j + (1 << i)] % MOD;
-                auto u = P[j];
-                P[j] = (u + t) % MOD;
-                P[j + (1 << i)] = (u - t + MOD) % MOD;
-                base = base * ws[i] % MOD;
+    void operator()(vector<int> &P, bool inv = 0) {
+        int n = P.size(), lg = __builtin_ctz(n);
+        assert(__builtin_popcount(n) == 1);
+        for (int j = 1, i = 0; j < n - 1; ++j) {
+            for (int k = n >> 1; k > (i ^= k); k >>= 1); // !!!
+            if (j < i) swap(P[i], P[j]);
+        }
+        for (int L = 2; L <= n; L <<= 1) {
+            int dx = n / L, dl = L >> 1;
+            for (int k = 0; k < n; k += L) {
+                for (int j = k, x = 0; j < k + dl; j++, x += dx) {
+                    int t = mul(ws[x], P[j + dl]);
+                    P[j + dl] = sub(P[j], t);
+                    P[j] = add(P[j], t);
+                }
             }
         }
+        if (inv) {
+            reverse(1 + all(P));
+            int invn = fpow(n, MOD - 2, MOD);
+            rep (i, 0, n) P[i] = mul(P[i], invn);
+        }
     }
-    if (inv) rep (i, 0, n) P[i] = P[i] * fpow(n, MOD - 2, MOD) % MOD;
-}
+};
 const int N = 1 << 20;
-const auto w = fpow(3, (MOD - 1) / N, MOD);
+ntt NTT(N);
